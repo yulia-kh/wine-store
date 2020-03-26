bb47ee4850e34e338cbbab111949b63d4995d857
yulia-kh <y.khisamutdinova@gmail.com>
2020-03-25 19:47:44 -0700

    add components and display them on the page

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <h3>Hello from Navbar</h3>
+            </nav>
+        )
+    }
+}
---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Product extends Component {
+    render() {
+        return (
+            <nav>
+                <h3>Hello from Product</h3>
+            </nav>
+        )
+    }
+}
